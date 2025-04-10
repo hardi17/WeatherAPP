dc9e68a56808aa00da36af9021b288ffe0fbda3d
Hardi Rachh <shahhardi71@gmail.com>
2025-04-10 16:00:54 +0100

    Added model class based on Weather api response.

---
+++ app/src/main/java/com/hardi/weatherapp/data/model/Condition.kt
@@ -0,0 +1,7 @@
+package com.hardi.weatherapp.data.model
+
+data class Condition(
+    val code: String,
+    val icon: String,
+    val text: String
+)
---
+++ app/src/main/java/com/hardi/weatherapp/data/model/Weather.kt
@@ -0,0 +1,6 @@
+package com.hardi.weatherapp.data.model
+
+data class Weather(
+    val current: Current,
+    val location: Location
+)
